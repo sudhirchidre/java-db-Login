9fa80b2cea22c720ea11c6416b8f438245d2d5e5
sudhirchidre <98510134+sudhirchidre@users.noreply.github.com>
2022-04-05 11:51:50 +0530

    Update DbUtil.java

--- src/main/java/com/example/DbUtil.java
+++ src/main/java/com/example/DbUtil.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class DbUtil {
 	static Connection con=null;
 	static String driver = "com.mysql.cj.jdbc.Driver";
-	static String url = "jdbc:mysql://lwplabs.cojh5a5pj8rf.us-east-1.rds.amazonaws.com:3306/lwplabs";
+	static String url = "jdbc:mysql://database-1.cqyulnoyzbxj.ap-south-1.rds.amazonaws.com";
 	static String username = "admin";
 	static String password = "welcome123";
 	
